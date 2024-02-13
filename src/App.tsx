90a5166d5c846a274f808da3f96f740b2e4facb2
Pi3tr3kk <90929380+Pi3tr3kk@users.noreply.github.com>
2024-02-13 23:05:13 +0100

    el primo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { ChakraProvider, theme } from '@chakra-ui/react'
+import { Button, Image, Text, Card } from '@chakra-ui/react'
+import { useState } from 'react'
+
+
+
+
+
 
 function App() {
+  const [type, changeType] = useState(false)
+
+  function wrongButton() {
+    alert('Źle skarbie kliknęłaś chyba, spróbuj jeszcze raz!')
+    changeType(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ChakraProvider theme={theme}>
+        <Card>
+          <Image className='center' id='serce' src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png'></Image>
+          <Text id='text' className='center' fontSize={24}>Bedziesz moją walentynką?</Text>
+          <br></br>
+          <Button onClick={() => alert('To super skarbie jestes tak, to moja walentynką!')} className='center' colorScheme='teal'>Tak</Button>
+          <br></br>
+          <Button isDisabled={type} onClick={() => wrongButton()} className='center' colorScheme='teal'>Nie</Button>
+        </Card>
+      </ChakraProvider>
+    </>
   );
 }
 
 export default App;
+
---
+++ src/components/navbar.tsx
@@ -0,0 +1,2 @@
+import React from "react";
+import { Link, Box, Flex, Text, Button, Stack } from "@chakra-ui/react";
